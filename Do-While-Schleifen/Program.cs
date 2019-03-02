ce3ed6a4dc9d96655980de6b2c66cb3408a7a940
Angelina <brubow@newvisionsproject.de>
2019-03-02 11:39:22 +0100

    initial commit

---
+++ Do-While-Schleifen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While_Schleifen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Do-While-Schleifen
+            int number = 7;
+
+            do
+            {
+                Console.WriteLine(number);
+                number += 1;
+            }
+            while (number <= 5);
+
+            Console.ReadKey();
+
+        }
+    }
+}
